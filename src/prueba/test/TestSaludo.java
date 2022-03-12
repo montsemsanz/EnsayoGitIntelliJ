5b3e3c0b14be2b2201efa22be42328116e5b3ab5
montsemsanz <montsemsanz@gmail.com>
2022-03-12 18:32:08 +0100

    Primer commit Saludo a Pepe

---
+++ src/prueba/test/TestSaludo.java
@@ -0,0 +1,15 @@
+package prueba.test;
+
+import prueba.modelo.Saludo;
+
+public class TestSaludo {
+
+	public static void main(String[] args) {
+		Saludo saludo = new Saludo();
+		System.out.println(saludo.saludar("Pepe"));
+		
+
+		
+	}
+
+}
