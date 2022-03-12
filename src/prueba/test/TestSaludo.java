4d8723d847f837408364cdc5e28dd23372cdc46f
montsemsanz <montsemsanz@gmail.com>
2022-03-12 19:54:13 +0100

    Segundo commit COLAB SAludo a Ana

--- src/prueba/test/TestSaludo.java
+++ src/prueba/test/TestSaludo.java
@@ -7,6 +7,7 @@ public class TestSaludo {
 	public static void main(String[] args) {
 		Saludo saludo = new Saludo();
 		System.out.println(saludo.saludar("Pepe"));
+		System.out.println(saludo.saludar("Ana"));
 		
 
 		
