bfdf65e01eeb1ac9396bd97c6645cecd0862a1a5
montsemsanz <montsemsanz@gmail.com>
2022-03-14 17:50:38 +0100

    Cuarto commit COLAB Saludo a Maite

--- src/prueba/test/TestSaludo.java
+++ src/prueba/test/TestSaludo.java
@@ -8,6 +8,7 @@ public class TestSaludo {
 		Saludo saludo = new Saludo();
 		System.out.println(saludo.saludar("Pepe"));
 		System.out.println(saludo.saludar("Ana"));
+		System.out.println(saludo.saludar("Maite"));
 		
 
 		
